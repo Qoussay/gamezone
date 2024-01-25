d859c4510bffd730be6e2fc5cb18a9f08c5c195f
Qoussay <qoussay.akkari@gmail.com>
2024-01-25 23:27:36 +0100

    added daisyui and tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="bg-primary">Welcome to SvelteKit</h1>
+<h1 class="bg-secondary">Welcome to SvelteKit</h1>
+<h1 class="bg-accent">Welcome to SvelteKit</h1>
+<h1 class="bg-neutral">Welcome to SvelteKit</h1>
+<h1 class="bg-base-100">Welcome to SvelteKit</h1>
+<h1 class="bg-base-200">Welcome to SvelteKit</h1>
+<h1 class="bg-base-300">Welcome to SvelteKit</h1>
+<h1 class="bg-info">Welcome to SvelteKit</h1>
+<h1 class="bg-success">Welcome to SvelteKit</h1>
+<h1 class="bg-warning">Welcome to SvelteKit</h1>
+<h1 class="bg-error">Welcome to SvelteKit</h1>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
